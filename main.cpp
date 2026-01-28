0f1a9a6ce7e72e8b360087024d2f8190f4cb6e7f
SgeniusJ <stavros@laptop.localdomain>
2026-01-28 14:46:31 +0200

    Updated the main.cpp file

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int n1;
+int n2;
+cin>>n1;
+cin>>n2;
+if (n1>n2){
+	cout<<"Bigger is:"<<n1;}
+else if(n1<n2){
+	cout<<"Bigger is:"<<n2;}
+else{cout<<"Equal";}
+
+return 0;}
+
+
+
+
+
+
+
+
+
+
